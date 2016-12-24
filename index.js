d78fbf07bdc22a9351930532aa0f21ed3f241ff8
Stephen <raphael.appsoluteperfection@gmail.com>
2016-12-24 15:29:10 -0600

    Using promises instead of passed in functions

--- index.js
+++ index.js
@@ -3,31 +3,33 @@
     
     const address = require('address');
 
-    exports.scan = (onFound, range, onSuccess, onError) => {
-        let myIp = address.ip();
-        let lastDotIndex = myIp.lastIndexOf('.');
-        let baseIp = myIp.substring(0, lastDotIndex);
-        let ranges = range.split('-');
-        if (ranges.length !== 2) {
-            throw new Error('Expected a range string of the form {start}-{end}');
-        }
-        let start = ranges[0].trim();
-        let end = ranges[1].trim();
-        if (end < start) {
-            throw new Error('Start range must be less than end range');
-        }
-        for (var i = start; i <= end; i++) {
-            let ip = `${baseIp}.${i}`; 
-            if (onFound(ip)) {
-                if (onSuccess) {
-                    onSuccess(ip);
-                }
-            } else {
-                if (onError) {
-                    onError(ip);
+    exports.scan = (onFound, range) => {
+        return new Promise((resolve, reject) => {
+            let myIp = address.ip();
+            let lastDotIndex = myIp.lastIndexOf('.');
+            let baseIp = myIp.substring(0, lastDotIndex);
+            let ranges = range.split('-');
+            if (ranges.length !== 2) {
+                throw new Error('Expected a range string of the form {start}-{end}');
+            }
+            let start = ranges[0].trim();
+            let end = ranges[1].trim();
+            if (end < start) {
+                throw new Error('Start range must be less than end range');
+            }
+            for (var i = start; i <= end; i++) {
+                let ip = `${baseIp}.${i}`; 
+                if (onFound(ip)) {
+                    if (resolve) {
+                        resolve(ip);
+                    }
+                } else {
+                    if (reject) {
+                        reject(ip);
+                    }
                 }
             }
-        }
+        });
     };
 
 })();
